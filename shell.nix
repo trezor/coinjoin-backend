89698b40e2136920dd84943f5d66bb207ee7f298
Szymon Lesisz <sz.lesisz@gmail.com>
2022-10-17 16:53:31 +0200

    chore: update shell.nix nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-# the last successful build of nixpkgs-unstable as of 2021-11-16 compatible to trezor-suite
+# the last successful build of nixpkgs-unstable as of 2022-05-31
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/5cb226a06c49f7a2d02863d0b5786a310599df6b.tar.gz";
-    sha256 = "0dzz207swwm5m0dyibhxg5psccrcqfh1lzkmzzfns27wc4ria6z3";
+    url = "https://github.com/NixOS/nixpkgs/archive/b62ada430501de88dfbb08cea4eb98ead3a5e3e7.tar.gz";
+    sha256 = "1ppaixbncqyvy2ixskwvzggjsw20j77iy3aqyk4749dvkx0ml27f";
   })
 { };
 
