9d55d422d5ddf4d8afc703a0043fd68b87162e09
Szymon Lesisz <sz.lesisz@gmail.com>
2024-01-02 13:03:05 +0100

    chore: update build of nixpkgs
    remove dotnet tools, build is done in docker

--- shell.nix
+++ shell.nix
@@ -1,27 +1,20 @@
-# the last successful build of nixpkgs-unstable as of 2023-02-28
+# the last successful build of nixos-unstable as of 2023-10-30
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/7785526659fe5885abbd88a85a23a11bd0617e3c.tar.gz";
-    sha256 = "0sjsy3jihhdmck6hn9lizwvk25kzf5ags1p21xqqn3kj7fv5ax9x";
+    url = "https://github.com/NixOS/nixpkgs/archive/63678e9f3d3afecfeafa0acead6239cdb447574c.tar.gz";
+    sha256 = "0l9b5w9riwhnf80w233plb4y028y2psr6gm8avdkwg7jvlga2j41";
   })
 { };
 
 stdenv.mkDerivation rec {
   name = "coinjoin-backend-dev";
   buildInputs = [
-    stdenv.cc.cc.lib
-    openssl
-    dotnet-sdk_8
     docker
-    fontconfig
-    fontconfig.lib
     git
     xorg.xhost
     xorg.libX11
     xorg.libX11.dev
     xorg.libICE
     xorg.libSM
-    zlib
   ];
-  LD_LIBRARY_PATH = "${(import <nixpkgs> { }).lib.makeLibraryPath buildInputs}";
 }
